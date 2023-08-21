207cca6ae6fa129801ef0960fa5a175c54baf99f
iotzbruhh <87367919+Itziotzbruh@users.noreply.github.com>
2023-08-21 04:08:31 +0100

    Commit from TeleportHQ

---
+++ tailwind.config.js
@@ -0,0 +1,79 @@
+module.exports = {
+  "theme": {
+    "extend": {
+      "colors": {
+        "Success": {
+          "300": "#199033",
+          "500": "#32A94C",
+          "700": "#4CC366"
+        },
+        "blue": {
+          "500": "#17c1e8ff"
+        },
+        "Gray": {
+          "500": "#595959",
+          "700": "#999999",
+          "800": "#cbcbcbff",
+          "900": "#D9D9D9",
+          "Black": "#000000",
+          "White": "#FFFFFF"
+        },
+        "Danger": {
+          "300": "#A22020",
+          "500": "#BF2626",
+          "700": "#E14747"
+        },
+        "Primary": {
+          "100": "#003EB3",
+          "300": "#0074F0",
+          "500": "#14A9FF",
+          "700": "#85DCFF"
+        },
+        "secondary": {
+          "200": "#8392abff",
+          "500": "#344767ff",
+          "600": "#252f40ff",
+          "700": "#20273fff"
+        },
+        "foreground": "#000000",
+        "background": "#FFFFFF"
+      },
+      "spacing": {
+        "Unit": "8px",
+        "triplequarterunit": "0.75rem",
+        "threeunits": "48px",
+        "oneandhalfunits": "24px",
+        "TripleUnit": "24px",
+        "halfunit8": "0.5rem",
+        "halfunit1": "0.5rem",
+        "unitandahalfunit": "1.5rem",
+        "FiveUnits": "40px",
+        "HalfUnit": "4px",
+        "TenUnits": "80px",
+        "unit1": "1rem",
+        "FourUnits": "32px",
+        "unit9": "1rem",
+        "sixunits": "6rem",
+        "DoubleUnit": "16px"
+      },
+      "borderRadius": {
+        "Radius 4": "4px",
+        "Round": "50%",
+        "radius1": "1rem",
+        "Radius 8": "8px",
+        "Radius 2": "2px",
+        "radius5": "0.5rem",
+        "radius75": "0.75rem"
+      },
+      "scale": {
+        "MaxContent": "1414px",
+        "unit": "1rem"
+      }
+    }
+  },
+  "plugins": [],
+  "content": [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./*.html"
+  ]
+}
